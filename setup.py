2f6e16420b83a829a699eca7e169d17d4313dcbf
Marcus D Sherman <betteridiot@users.noreply.github.com>
2019-07-22 12:29:56 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from os import path
 from setuptools import setup
 
+
 def version_info():
     here = path.abspath(path.dirname(__file__))
     with open(path.join(here, 'version'), encoding = 'utf-8') as vers:
@@ -19,11 +20,11 @@ setup(
     author = "Adam Diehl",
     author_email = "adadiehl@umich.edu",
     description = "Prediction of lineage-specific gain and loss of sequence elements using phylogenetic maximum parsimony.",
-    long_description = readme(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/adadiehl/mapGL",
-    packages=setuptools.find_packages(),
-    classifiers=[
+    long_description = readme(), # https://packaging.python.org/guides/making-a-pypi-friendly-readme/#including-your-readme-in-your-package-s-metadata
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/adadiehl/mapGL",
+    packages = setuptools.find_packages(),
+    classifiers = [
         "Programming Language :: Python :: 3",
         "Programming Language :: Cython",
         "License :: OSI Approved :: MIT License",
