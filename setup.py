e23d8bee533c9dfb96b9d63a5dc74a160f728de5
Marcus D Sherman <betteridiot@users.noreply.github.com>
2019-07-22 12:28:10 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,31 @@
-import setuptools
+from os import path
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+def version_info():
+    here = path.abspath(path.dirname(__file__))
+    with open(path.join(here, 'version'), encoding = 'utf-8') as vers:
+        return vers.read().strip()
 
-setuptools.setup(
-    name="mapGL",
-    version="0.0.1",
-    author="Adam Diehl",
-    author_email="adadiehl@umich.edu",
-    description="Prediction of lineage-specific gain and loss of sequence elements using phylogenetic maximum parsimony.",
-    long_description=long_description,
+    
+def readme():
+    here = path.abspath(path.dirname(__file__))
+    with open(path.join(here, 'README.md'), encoding='utf-8') as md:
+        return md.read()
+
+    
+setup(
+    name = "mapGL",
+    version = version_info(),
+    author = "Adam Diehl",
+    author_email = "adadiehl@umich.edu",
+    description = "Prediction of lineage-specific gain and loss of sequence elements using phylogenetic maximum parsimony.",
+    long_description = readme(),
     long_description_content_type="text/markdown",
     url="https://github.com/adadiehl/mapGL",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Cython",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
